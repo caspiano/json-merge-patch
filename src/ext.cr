d063067d1edce9480f27d79355457cbe20b1570b
Caspian Baska <email@caspian.computer>
2022-06-12 01:33:14 +1000

    Use absolute symbol paths in `ext.cr`

--- src/ext.cr
+++ src/ext.cr
@@ -1,9 +1,9 @@
 require "./json-merge-patch"
 
 struct JSON::Any
-  include JSON::RFC7396
+  include ::JSON::Any::MergePatch
 
   def merge(other : ::JSON::Any) : ::JSON::Any
-    self.class.merge(self, other)
+    ::JSON::Any::MergePatch.merge(self, other)
   end
 end
